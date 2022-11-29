f495b7c0723445c3d35cbcf7b25cfe7382209497
Ioana2002 <maria.antohi@stud.ubbcluj.ro>
2022-11-29 16:26:01 +0200

    Laborator8

--- Antohi_Ioana_Lab7/App.xaml.cs
+++ Antohi_Ioana_Lab7/App.xaml.cs
@@ -1,8 +1,25 @@
 ﻿namespace Antohi_Ioana_Lab7;
+using System;
+using System.IO;
+using Antohi_Ioana_Lab7.Data;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+    public App()
 	{
 		InitializeComponent();
 
