9f3d27550deed7f42e3e7939f013f4a3bf575e0f
Ioana2002 <maria.antohi@stud.ubbcluj.ro>
2022-12-20 15:55:50 +0200

    lab

--- Antohi_Ioana_Lab7/Data/ShoppingListDatabase.cs
+++ Antohi_Ioana_Lab7/Data/ShoppingListDatabase.cs
@@ -17,6 +17,7 @@ namespace Antohi_Ioana_Lab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
         }
         public Task<List<ShopList>> GetShopListsAsync()
         {
@@ -93,6 +94,21 @@ namespace Antohi_Ioana_Lab7.Data
         public Task<List<ListProduct>> GetListProducts()
         {
             return _database.QueryAsync<ListProduct>("select * from ListProduct");
+        }
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
         }
 
     }
